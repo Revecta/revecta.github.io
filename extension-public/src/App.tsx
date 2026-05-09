fdef66ca5631ed28aca751b69e92b7a40b941f60
Michele Lazzari <Michele.lazzari@aol.com>
2026-05-09 09:01:19 +0200

    chore: bump version to 1.0.1

--- extension-public/src/App.tsx
+++ extension-public/src/App.tsx
@@ -118,7 +118,7 @@ function App() {
           <img src="./icons/icon48.png" alt="CodiceAmico" className="logo-img" />
           <div className="logo-meta">
             <span className="logo-text">Codice<span className="logo-accent">Amico</span></span>
-            {!isExtension && <span className="version-pill">v1.0.0</span>}
+            {!isExtension && <span className="version-pill">v1.0.1</span>}
           </div>
         </div>
 
